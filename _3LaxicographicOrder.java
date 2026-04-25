a1f331bc30c5458fb5df85a34aecfcb6e06ddefd
Akash Dwivedi <akashdwi17@gmail.com>
2026-04-25 07:43:19 +0530

    Laxicographic Order

---
+++ _3LaxicographicOrder.java
@@ -0,0 +1,15 @@
+public class _3LaxicographicOrder {
+    public static void laxicographicOrder (String fruits[]){
+        String largest = fruits[0];
+        for (int i=1; i<fruits.length; i++){
+            if (largest.compareTo(fruits[i]) < 0 ){
+                largest = fruits[i];
+            }
+        }
+        System.out.println (largest);
+    }
+    public static void main(String[] args) {
+        String fruits[] = {"apple", "mango", "banana"};
+        laxicographicOrder(fruits);
+    }
+}
